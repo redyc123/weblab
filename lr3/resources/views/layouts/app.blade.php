64ecd048034aea53fd4d48b0eec6024196bbb58a
redyc123 <redyc123@gmail.com>
2025-12-10 18:59:47 +0500

    fix

--- lr3/resources/views/layouts/app.blade.php
+++ lr3/resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
 <body>
 <nav class="navbar navbar-expand-md navbar-light bg-light mb-4">
     <div class="container">
-        <a class="navbar-brand" href="{{ route('items.index') }}">LR3</a>
+        <a class="navbar-brand" href="{{ route('items.index') }}">ТУРЫ</a>
         <div>
             <a class="btn btn-primary" href="{{ route('items.create') }}">Добавить</a>
         </div>
